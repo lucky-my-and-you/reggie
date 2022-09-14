f6e09d9031e7a18dae362106f4ca78a5b952c388
zhang <1326941494@qq.com>
2022-09-14 14:52:19 +0800

    不要redis验证登录

--- src/main/java/com/zhang/reggie/controller/UserController.java
+++ src/main/java/com/zhang/reggie/controller/UserController.java
@@ -26,8 +26,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private RedisTemplate redisTemplate;
+//    @Autowired
+//    private RedisTemplate redisTemplate;
 
     /**
      * 发送手机短信验证码
@@ -41,15 +41,15 @@ public class UserController {
         //获取手机号
         String phone = user.getPhone();
         //随机生成验证码
-        String code = ValidateCodeUtils.generateValidateCode(4).toString();
-        System.out.println(code);
+//        String code = ValidateCodeUtils.generateValidateCode(4).toString();
+//        System.out.println(code);
 
         if (StringUtils.isNotEmpty(phone)) {
 
             //需要将生成的验证码保存到Session
-            // session.setAttribute(phone,code);
+             session.setAttribute("phone",phone);
             //将验证码存入redis中， 保存时间五分钟
-            redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
+//            redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
             return R.success("手机验证码短信发送成功");
         }
         return R.error("短信发送失败");
@@ -70,16 +70,16 @@ public class UserController {
         String phone = map.get("phone").toString();
 
         //获取验证码
-        String code = map.get("code").toString();
+//        String code = map.get("code").toString();
 
         //从Session中获取保存的验证码
-        //Object codeInSession = session.getAttribute(phone);
+        Object codeInSession = session.getAttribute("phone");
 
         //从redis中获取缓存
-        Object codeRedis = redisTemplate.opsForValue().get(phone);
+//        Object codeRedis = redisTemplate.opsForValue().get(phone);
 
         //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
-        if (codeRedis != null && codeRedis.equals(code)) {
+        if (codeInSession != null && codeInSession.equals(phone)) {
             //如果能够比对成功，说明登录成功
 
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
@@ -94,7 +94,7 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user", user.getId());
-            redisTemplate.delete(phone);
+//            redisTemplate.delete(phone);
             return R.success(user);
         }
         return R.error("登录失败");
